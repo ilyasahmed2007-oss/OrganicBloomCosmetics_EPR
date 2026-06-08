120f8a7c9249e003ce93a885aa3769b5934ff271
ilyasahmed2007-oss <ilyasahmed2007@gmail.com>
2026-06-08 20:47:37 +0500

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController user = TextEditingController();
+  TextEditingController pass = TextEditingController();
+
+  void login() {
+    if (user.text == "admin" && pass.text == "1234") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const DashboardScreen()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Wrong login details")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Organic Bloom ERP",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+
+              TextField(
+                controller: user,
+                decoration: const InputDecoration(labelText: "Username"),
+              ),
+
+              TextField(
+                controller: pass,
+                obscureText: true,
+                decoration: const InputDecoration(labelText: "Password"),
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: login,
+                child: const Text("Login"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
