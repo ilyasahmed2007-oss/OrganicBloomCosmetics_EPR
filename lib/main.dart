b5226f60f8d25feb8d7e6630c2d788016716c67d
ilyasahmed2007-oss <ilyasahmed2007@gmail.com>
2026-06-08 20:46:42 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
 
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Organic Bloom ERP',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
