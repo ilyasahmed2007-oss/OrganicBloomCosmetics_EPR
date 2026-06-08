3a61a5cd700ee68082ab1a6fe023206e24091fd7
ilyasahmed2007-oss <ilyasahmed2007@gmail.com>
2026-06-08 20:48:29 +0500

    Update dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  Widget card(String title, IconData icon) {
+    return Card(
+      elevation: 4,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 40, color: Colors.green),
+            const SizedBox(height: 10),
+            Text(title),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        backgroundColor: Colors.green,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(10),
+        children: [
+          card("Sales", Icons.point_of_sale),
+          card("Stock", Icons.inventory),
+          card("Manufacturing", Icons.precision_manufacturing),
+          card("Purchase", Icons.shopping_cart),
+        ],
+      ),
+    );
+  }
+}
